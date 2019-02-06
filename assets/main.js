925491ddb646d597fa9419ddb0b7629a6857173a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-06 10:47:52 +0100

    fixed template function

--- assets/main.js
+++ assets/main.js
@@ -4,19 +4,19 @@ class TrackList {
   // Creating our Class
   constructor(domSelector, data) {
     // Getting a domelement
-    this.container = document.querySelector(domSelector)
+    this.container = document.querySelector(domSelector);
     // Store my data
-    this.data = data
+    this.data = data;
     // Represents the currently displayed data
-    this.viewData = data
+    this.viewData = data;
 
     // Show stuff
-    this.render()
+    this.render();
   }
 
   modViewData(newData) {
-    this.viewData = newData
-    this.render()
+    this.viewData = newData;
+    this.render();
   }
 
   template(music) {
@@ -24,6 +24,19 @@ class TrackList {
     // For now we just assume that all data is there and that it is
     // from datatype string
     // TODO: create a template function
+    const htmlString = music
+      .map(track => {
+        return `
+      <div class="row">
+      <img src="${track.artworkUrl100}" />
+      <div>${track.trackName}</div>
+      <div>${track.artistName}</div>
+      <div>${track.price}</div>
+      </div>
+      `;
+      })
+      .join("");
+    return htmlString;
   }
 
   sortPricing() {
@@ -32,19 +45,19 @@ class TrackList {
 
   render() {
     // Out put will hold the complete view
-    let output = ""
+    let output = "";
 
     // Setting up data for our view
-    const header = "<h1>My Tracks</h1>"
+    const header = "<h1>My Tracks</h1>";
     // template methode accepts data to view and returns html string
-    const template = this.template(this.viewData)
+    const template = this.template(this.viewData);
     // Adding data in to our view !Order Matters!
-    output += header
-    output += "<p>Data from iTunes</p>"
-    output += template
+    output += header;
+    output += "<p>Data from iTunes</p>";
+    output += template;
     // Assinging view in to innerHTML of our domElement form the constructor
-    this.container.innerHTML = output
+    this.container.innerHTML = output;
   }
 }
 
-const myTrackList = new TrackList("#tracks", music)
+const myTrackList = new TrackList("#tracks", music);
